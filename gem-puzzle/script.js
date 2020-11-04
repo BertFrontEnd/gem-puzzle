53c3bb39e3201404765852bf4dc29f167a92e2a9
BertFrontEnd <Berton@yandex.ru>
2020-11-04 23:47:45 +0300

    feat: add controls

--- gem-puzzle/script.js
+++ gem-puzzle/script.js
@@ -1,9 +1,8 @@
 import { toReplace, toDrag } from './assets/module/elements.js';
-import {
-  dragoverHandler,
-  dropHandler,
-  dragstartHandler,
-  dragendHandler,
-} from './assets/module/functions.js';
+import { dragoverHandler, dropHandler, dragstartHandler, dragendHandler } from './assets/module/handlers.js';
 
-toReplace.addEventListener('dragstart', dragstartHandler);
+/* toReplace.addEventListener('dragstart', dragstartHandler);
+toReplace.addEventListener('dragend', dragendHandler);
+
+toDrag.addEventListener('drop', dropHandler);
+toDrag.addEventListener('dragover', dragoverHandler); */
